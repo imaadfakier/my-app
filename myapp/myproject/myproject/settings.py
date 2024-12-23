4ec4e64d4c0546f9158bffd67152495055676f29
Imaad Fakier <79690509+imaadfakier@users.noreply.github.com>
2024-12-23 16:20:04 +0200

    Update settings.py
    Updated SECRET_KEY with the new key's value using the appropriate environment variable. Thus, abiding by security best practices.

--- myapp/myproject/myproject/settings.py
+++ myapp/myproject/myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ookr21)i_7i_+bzz9xg9m#zmvkl$)a&78v@p8auz=$=sytp+!q'
+SECRET_KEY = os.environ("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
